dad7c39aece2265e3cd136388d9517af01f5abe4
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-14 13:04:38 -0500

    Create XDebugListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Listener/XDebugListener.php
@@ -0,0 +1,58 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Listener;
+
+use Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Event\AbstractEvent;
+use Ghostwriter\GhostwriterPhpDockerTemplateUpdater\PhpVersion;
+use Throwable;
+
+final class XDebugListener extends AbstractListener
+{
+    /**
+     * @throws Throwable
+     */
+    public function __invoke(AbstractEvent $event): void
+    {
+        $from = $event->getFrom();
+        $to = $event->getTo();
+
+        $this->checkout(self::BRANCH_MAIN);
+        foreach (PhpVersion::SUPPORTED as $phpVersion) {
+            if (! $this->isBranch(self::BRANCH_MAIN)) {
+                $this->checkout(self::BRANCH_MAIN);
+            }
+
+            $phpVersionDir = sprintf('%s/%s', $this->gitRepository->getWorkingDir(), $phpVersion);
+            if (! is_dir($phpVersionDir)) {
+                continue;
+            }
+
+            $dockerFile = $phpVersionDir . '/Dockerfile';
+            if (! is_file($dockerFile)) {
+                continue;
+            }
+
+            $dockerFileContents = file_get_contents($dockerFile);
+            if (1 === preg_match(sprintf('#XDEBUG_VERSION\s%s#', $from), $dockerFileContents)) {
+                $branchName = 'feature/php-' . $phpVersion . '/bump-xdebug-from-' . $from . '-to-' . $to;
+
+                $this->hasBranch($branchName) ?
+                    $this->checkout($branchName) :
+                    $this->checkout(self::BRANCH_MAIN, $branchName);
+
+                file_put_contents(
+                    $dockerFile,
+                    str_replace('_VERSION ' . $from, '_VERSION ' . $to, $dockerFileContents)
+                );
+
+                if ($this->hasChanges()) {
+                    $this->add($dockerFile);
+                    $this->commit(sprintf('[PHP %s]Bump Xdebug from %s to %s', $phpVersion, $from, $to));
+                }
+            }
+        }
+    }
+}

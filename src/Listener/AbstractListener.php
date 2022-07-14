ad459ebe3db68c03548c9d8dbd1720be08b36784
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-14 11:52:41 -0500

    Update AbstractListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/AbstractListener.php
+++ src/Listener/AbstractListener.php
@@ -6,20 +6,35 @@ declare(strict_types=1);
 namespace Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Listener;
 
 use Gitonomy\Git\Repository;
+use Symfony\Component\Console\Style\SymfonyStyle;
 
 abstract class AbstractListener
 {
-    public function __construct(protected Repository $gitRepository)
+    public function __construct(
+        protected Repository $gitRepository,
+        protected SymfonyStyle $output
+    )
     {
+    }
+
+    public function checkout(string $revision, string $branch = null): void
+    {
+        $checkoutBranch = $branch ?? $revision;
+        $this->output->info('git:checkout - ' . $checkoutBranch);
+        $this->gitRepository
+            ->getWorkingCopy()
+            ->checkout($revision, $branch);
     }
 
     public function add(string $filePattern): string
     {
+        $this->output->info('git:add - ' . $filePattern);
         return $this->gitRepository->run('add', [$filePattern]);
     }
 
     public function commit(array $args): string
     {
+        $this->output->info('git:commit - ' . json_encode($args));
         return $this->gitRepository->run('commit', [...$args]);
     }
 

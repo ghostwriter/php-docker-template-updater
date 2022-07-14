3bf557a792c7389aaa15f24ff7040010da5cb310
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-14 13:04:48 -0500

    Update ComposerListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/ComposerListener.php
+++ src/Listener/ComposerListener.php
@@ -7,7 +7,6 @@ namespace Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Listener;
 
 use Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Event\AbstractEvent;
 use Ghostwriter\GhostwriterPhpDockerTemplateUpdater\PhpVersion;
-use InvalidArgumentException;
 use Throwable;
 
 final class ComposerListener extends AbstractListener
@@ -17,29 +16,15 @@ final class ComposerListener extends AbstractListener
      */
     public function __invoke(AbstractEvent $event): void
     {
-        $input = $event->getInput();
-        $output = $event->getOutput();
+        $from = $event->getFrom();
+        $to = $event->getTo();
 
-        $from = $input->getArgument('from');
-        $to = $input->getArgument('to');
-        // TODO: Should i add a `--dry-run` option?
-        // $dryRun = $input->getOption('dry-run');
-
-
-        if (! is_string($from) || '' === $from) {
-            throw new InvalidArgumentException('$from is invalid');
-        }
-
-        if (! is_string($to) || '' === $to) {
-            throw new InvalidArgumentException('$to is invalid');
-        }
-
-        $git = $this->gitRepository->getWorkingCopy();
-        $dir = $this->gitRepository->getWorkingDir();
-
-        $git->checkout('main');
         foreach (PhpVersion::SUPPORTED as $phpVersion) {
-            $phpVersionDir = sprintf('%s/%s', $dir, $phpVersion);
+            if (! $this->isBranch(self::BRANCH_MAIN)) {
+                $this->checkout(self::BRANCH_MAIN);
+            }
+
+            $phpVersionDir = sprintf('%s/%s', $this->gitRepository->getWorkingDir(), $phpVersion);
             if (! is_dir($phpVersionDir)) {
                 continue;
             }
@@ -50,17 +35,12 @@ final class ComposerListener extends AbstractListener
             }
 
             $dockerFileContents = file_get_contents($dockerFile);
-
-            if (1 === preg_match(sprintf('#_VERSION\s%s#', $from), $dockerFileContents, $matches)) {
+            if (1 === preg_match(sprintf('#_VERSION\s%s#', $from), $dockerFileContents)) {
                 $branchName = 'feature/php-' . $phpVersion . '/bump-composer-from-' . $from . '-to-' . $to;
 
-                if ($this->hasBranch($branchName)) {
-                    $output->writeln('checkoutOldBranch - ' . $branchName);
-                    $git->checkout($branchName);
-                } else {
-                    $output->writeln('checkoutNewBranch - ' . $branchName);
-                    $git->checkout('main', $branchName);
-                }
+                $this->hasBranch($branchName) ?
+                    $this->checkout($branchName) :
+                    $this->checkout(self::BRANCH_MAIN, $branchName);
 
                 file_put_contents(
                     $dockerFile,
@@ -69,22 +49,9 @@ final class ComposerListener extends AbstractListener
 
                 if ($this->hasChanges()) {
                     $this->add($dockerFile);
-                    $output->writeln('git:add - ' . $dockerFile);
-
-                    $this->commit([
-                        '--message',
-                        sprintf('[PHP %s]Bump composer/composer from %s to %s', $phpVersion, $from, $to),
-                        '--signoff',
-                        '--gpg-sign',
-                    ]);
-
-                    $output->writeln('git:commit - ' . $branchName);
+                    $this->commit(sprintf('[PHP %s]Bump composer/composer from %s to %s', $phpVersion, $from, $to));
                 }
-            } else {
-                $output->writeln($phpVersion . ' dockerFile does not content "' . $from . '" - ' . $dockerFile);
             }
-
-            $git->checkout('main');
         }
     }
 }

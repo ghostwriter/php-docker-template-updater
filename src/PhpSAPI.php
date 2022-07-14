87818fdadd80ac7ed3b6b11a81d7afe57280d04a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-14 13:04:33 -0500

    Update PhpSAPI.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/PhpSAPI.php
+++ src/PhpSAPI.php
@@ -6,9 +6,18 @@ namespace Ghostwriter\GhostwriterPhpDockerTemplateUpdater;
 
 final class PhpSAPI
 {
+    /**
+     * @var string
+     */
     public const PHP_CLI = 'cli';
 
+    /**
+     * @var string
+     */
     public const PHP_FPM = 'fpm';
 
+    /**
+     * @var string[]
+     */
     public const SUPPORTED = [self::PHP_CLI, self::PHP_FPM];
 }

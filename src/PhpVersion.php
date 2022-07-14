a0b650c639f173bddafc06152d9e67ebb0772fea
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-14 13:04:30 -0500

    Update PhpVersion.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/PhpVersion.php
+++ src/PhpVersion.php
@@ -6,24 +6,54 @@ namespace Ghostwriter\GhostwriterPhpDockerTemplateUpdater;
 
 final class PhpVersion
 {
+    /**
+     * @var string
+     */
     public const PHP_56 = '5.6';
 
+    /**
+     * @var string
+     */
     public const PHP_70 = '7.0';
 
+    /**
+     * @var string
+     */
     public const PHP_71 = '7.1';
 
+    /**
+     * @var string
+     */
     public const PHP_72 = '7.2';
 
+    /**
+     * @var string
+     */
     public const PHP_73 = '7.3';
 
+    /**
+     * @var string
+     */
     public const PHP_74 = '7.4';
 
+    /**
+     * @var string
+     */
     public const PHP_80 = '8.0';
 
+    /**
+     * @var string
+     */
     public const PHP_81 = '8.1';
 
+    /**
+     * @var string
+     */
     public const PHP_82 = '8.2';
 
+    /**
+     * @var string[]
+     */
     public const SUPPORTED = [
         self::PHP_56,
         self::PHP_70,

6112879154196c02f8d4bd23d98b20c4da077de6
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-22 11:32:35 -0500

    Update PhpVersionListener.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Listener/PhpVersionListener.php
+++ src/Listener/PhpVersionListener.php
@@ -37,9 +37,7 @@ final class PhpVersionListener extends AbstractListener
                 if (str_contains($dockerFileContents, sprintf($format, $from))) {
                     $branchName = $this->branchName($phpVersion, 'php-' . $type, $from, $to);
 
-                    $this->hasBranch($branchName) ?
-                        $this->checkout($branchName) :
-                        $this->checkout(self::BRANCH_MAIN, $branchName);
+                    $this->switch($branchName, ! $this->hasBranch($branchName));
 
                     file_put_contents(
                         $dockerFile,

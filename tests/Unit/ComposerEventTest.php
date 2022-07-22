0b4aae4e95aef3fb6277eacc640ba0a5add8d39e
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-22 14:09:14 -0500

    Create ComposerEventTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/ComposerEventTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Tests\Unit;
+
+
+use Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Event\ComposerEvent;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputDefinition;
+
+/**
+ * @coversDefaultClass \Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Event\ComposerEvent
+ *
+ * @internal
+ * @small
+ */
+final class ComposerEventTest extends AbstractTestCase
+{
+    /**
+     * @covers \Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Event\ComposerEvent::__construct
+     */
+    public function testTrue(): void
+    {
+        self::assertInstanceOf(
+            ComposerEvent::class,
+            new ComposerEvent(
+                new ArrayInput(
+                    [
+                        'from' => '1.2.3',
+                        'to' =>'1.2.4'
+                    ],
+                    new InputDefinition([
+                        new InputArgument('from', InputArgument::REQUIRED),
+                        new InputArgument('to', InputArgument::REQUIRED),
+                    ])
+                )
+            )
+        );
+    }
+}

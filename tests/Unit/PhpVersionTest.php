c54c32a3b4433ddafafd00aa8e55556c7eb1cc20
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-22 14:08:54 -0500

    Update PhpVersionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/PhpVersionTest.php
+++ tests/Unit/PhpVersionTest.php
@@ -4,16 +4,29 @@ declare(strict_types=1);
 
 namespace Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Tests\Unit;
 
+use Ghostwriter\GhostwriterPhpDockerTemplateUpdater\PhpVersion;
+
 /**
- * @internal
+ * @coversDefaultClass \Ghostwriter\GhostwriterPhpDockerTemplateUpdater\PhpVersion
  *
+ * @internal
  * @small
- * @coversNothing
  */
 final class PhpVersionTest extends AbstractTestCase
 {
+    /**
+     * @coversNothing
+     */
     public function testTrue(): void
     {
-        self::assertTrue(true);
+        self::assertContains(PhpVersion::PHP_56, PhpVersion::SUPPORTED);
+        self::assertContains(PhpVersion::PHP_70, PhpVersion::SUPPORTED);
+        self::assertContains(PhpVersion::PHP_71, PhpVersion::SUPPORTED);
+        self::assertContains(PhpVersion::PHP_72, PhpVersion::SUPPORTED);
+        self::assertContains(PhpVersion::PHP_73, PhpVersion::SUPPORTED);
+        self::assertContains(PhpVersion::PHP_74, PhpVersion::SUPPORTED);
+        self::assertContains(PhpVersion::PHP_80, PhpVersion::SUPPORTED);
+        self::assertContains(PhpVersion::PHP_81, PhpVersion::SUPPORTED);
+        self::assertContains(PhpVersion::PHP_82, PhpVersion::SUPPORTED);
     }
 }

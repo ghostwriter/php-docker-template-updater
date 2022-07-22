7683fc8d6f22bdf23ecbd83eaf8c59b235710eff
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2022-07-22 13:39:12 -0500

    Create PhpVersionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/PhpVersionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\GhostwriterPhpDockerTemplateUpdater\Tests\Unit;
+
+/**
+ * @internal
+ *
+ * @small
+ * @coversNothing
+ */
+final class PhpVersionTest extends AbstractTestCase
+{
+    public function testTrue(): void
+    {
+        self::assertTrue(true);
+    }
+}
